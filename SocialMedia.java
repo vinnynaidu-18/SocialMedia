1281d7903300ba93fa75d2e341109c540951c556
vinnynaidu-18 <vinnynaidu128@gmail.com>
2024-02-11 12:53:08 +0530

    logging2

---
+++ SocialMedia.java
@@ -0,0 +1,16 @@
+public class SocialMedia {
+        private String userName;
+        private String password;
+        public SocialMedia(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+
+        }
+
+        boolean login(String userName, String password) {
+            return (this.userName.equals(userName) && this.password.equals(password));
+
+        }
+    }
+
+
